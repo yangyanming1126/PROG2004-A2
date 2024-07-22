d2f16c3613ac2c8100072dcefb4b343ca1ce2a46
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-22 17:38:56 +1000

    pt1.ride

--- YanmingYang-A2/src/Ride.java
+++ YanmingYang-A2/src/Ride.java
@@ -1,2 +1,39 @@
 public class Ride {
+    private String name;
+    private int capacity;
+    private Employee operator;
+
+    public Ride(){
+    }
+
+    public Ride(String name, int capacity, Employee operator) {
+        this.name = name;
+        this.capacity = capacity;
+        this.operator = operator;
+    }
+
+    public  String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+        System.out.println("Operator " + operator.getName() + "has been assigned to the ride " + this.name);
+    }
 }
