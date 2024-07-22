dca2b98650fbc771cd0920c089b2ad68e41837cf
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-22 17:40:04 +1000

    pt1.Assessment2

--- YanmingYang-A2/src/AssessmentTwo.java
+++ YanmingYang-A2/src/AssessmentTwo.java
@@ -1,2 +1,23 @@
 public class AssessmentTwo {
+    public static void main(String[] args) {
+        // 你的代码将在这里开始
+    }
+
+    public void partThree(){
+    }
+
+    public void partFourA(){
+    }
+
+    public void partFourB(){
+    }
+
+    public void partFive(){
+    }
+
+    public void partSix(){
+    }
+
+    public void partSeven(){
+    }
 }
