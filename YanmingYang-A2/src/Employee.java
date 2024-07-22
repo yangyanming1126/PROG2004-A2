b0d67a2a2634de56d0ebacfe8865f9fc99c98f20
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-22 17:13:57 +1000

    pt1 employee

--- YanmingYang-A2/src/Employee.java
+++ YanmingYang-A2/src/Employee.java
@@ -1,2 +1,29 @@
-public class Employee {
+public class Employee extends Person {
+    private String position;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, String address, String position, double salary) {
+        super(name, age, address);
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
