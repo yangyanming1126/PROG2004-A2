9ef0669bee336ff638b390cb57d76d140a3fad6c
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-22 17:30:35 +1000

    pt1.visitor

--- YanmingYang-A2/src/Visitor.java
+++ YanmingYang-A2/src/Visitor.java
@@ -1,2 +1,29 @@
-public class Visitor {
+public class Visitor extends Person{
+    private String ticketType;
+    private boolean hasVIPAccess;
+
+    public Visitor() {
+    }
+
+    public Visitor(String name, int age, String address, String ticketType, boolean hasVIPAccess) {
+        super(name, age, address);
+        this.ticketType = ticketType;
+        this.hasVIPAccess = hasVIPAccess;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public boolean isHasVIPAccess() {
+        return hasVIPAccess;
+    }
+
+    public void setHasVIPAccess(boolean hasVIPAccess) {
+        this.hasVIPAccess = hasVIPAccess;
+    }
 }
