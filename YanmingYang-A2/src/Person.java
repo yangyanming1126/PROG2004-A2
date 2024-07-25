bfae259bd1cf39fe869d56128d71e5d8a3c9a0e8
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-25 21:13:20 +1000

    update

--- YanmingYang-A2/src/Person.java
+++ YanmingYang-A2/src/Person.java
@@ -1,12 +1,12 @@
-public class Person {
+public abstract class Person {
     private String name;
     private int age;
     private String address;
 
-    public Person(){
-
+    public Person() {
     }
-    public Person(String name,int age, String address) {
+
+    public Person(String name, int age, String address) {
         this.name = name;
         this.age = age;
         this.address = address;
@@ -15,12 +15,15 @@ public class Person {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
