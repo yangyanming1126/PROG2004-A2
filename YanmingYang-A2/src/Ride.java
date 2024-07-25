fd26444766a8fc34dc56d6428688e09205986709
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-25 21:41:32 +1000

    Update ride

--- YanmingYang-A2/src/Ride.java
+++ YanmingYang-A2/src/Ride.java
@@ -1,15 +1,25 @@
-public class Ride {
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Ride implements RideInterface {
     private String name;
     private int capacity;
     private Employee operator;
+    private Queue<Visitor> visitorQueue;
+    private LinkedList<Visitor> rideHistory;
 
     public Ride(){
+        visitorQueue = new LinkedList<>();
+        rideHistory = new LinkedList<>();
     }
 
     public Ride(String name, int capacity, Employee operator) {
         this.name = name;
         this.capacity = capacity;
         this.operator = operator;
+        this.visitorQueue = new LinkedList<>();
+        this.rideHistory = new LinkedList<>();
     }
 
     public  String getName() {
@@ -34,6 +44,59 @@ public class Ride {
 
     public void setOperator(Employee operator) {
         this.operator = operator;
-        System.out.println("Operator " + operator.getName() + "has been assigned to the ride " + this.name);
+    }
+    public void assignOperator(Employee operator) {
+        this.operator = operator;
+        System.out.println("Operator " + operator.getName() + " has been assigned to the ride " + this.name);
+    }
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        visitorQueue.add(visitor);
+        System.out.println("Visitor " + visitor.getName() + " has been added to the queue.");
+    }
+
+    @Override
+    public void removeVisitorFromQueue(Visitor visitor) {
+        if (visitorQueue.remove(visitor)) {
+            System.out.println("Visitor " + visitor.getName() + " has been removed from the queue.");
+        } else {
+            System.out.println("Visitor " + visitor.getName() + " is not in the queue.");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Current visitors in queue:");
+        for (Visitor visitor : visitorQueue) {
+            System.out.println(visitor.getName());
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        if (operator == null) {
+            System.out.println("No operator assigned to the ride. Cannot run the ride.");
+            return;
+        }
+        if (visitorQueue.isEmpty()) {
+            System.out.println("No visitors in the queue. Cannot run the ride.");
+            return;
+        }
+        int riders = Math.min(capacity, visitorQueue.size());
+        System.out.println("Running one cycle with " + riders + " visitors.");
+        for (int i = 0; i < riders; i++) {
+            Visitor visitor = visitorQueue.poll();
+            rideHistory.add(visitor);
+        }
+    }
+
+    @Override
+    public void printRideHistory() {
+        System.out.println("Visitors who have taken the ride:");
+        for (Visitor visitor : rideHistory) {
+            System.out.println(visitor.getName());
+        }
     }
 }
+
