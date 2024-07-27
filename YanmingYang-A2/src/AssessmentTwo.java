896534fabe7dee25ca936a1202e21cd2722516c2
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-27 16:00:42 +1000

    Update AssessmentTwo.java

--- YanmingYang-A2/src/AssessmentTwo.java
+++ YanmingYang-A2/src/AssessmentTwo.java
@@ -84,6 +84,43 @@ public class AssessmentTwo {
     }
 
     public void partFive(){
+        Ride rollerCoaster = new Ride("Roller Coaster", 3, new Employee("John", 30, "789 Boulevard", "Operator", 3000));
+
+        // 创建一些Visitor对象
+        Visitor visitor1 = new Visitor("Jack", 25, "123 Street", "Regular", false);
+        Visitor visitor2 = new Visitor("Sharon", 30, "456 Avenue", "Regular", false);
+        Visitor visitor3 = new Visitor("Benny", 22, "789 Boulevard", "Regular", false);
+        Visitor visitor4 = new Visitor("Leo", 28, "101 Road", "VIP", true);
+        Visitor visitor5 = new Visitor("Nehemia", 35, "202 Lane", "Regular", false);
+        Visitor visitor6 = new Visitor("Tom", 25, "123 Street", "Regular", false);
+        Visitor visitor7 = new Visitor("Sherly", 30, "456 Avenue", "Regular", false);
+        Visitor visitor8 = new Visitor("Ben", 22, "789 Boulevard", "Regular", false);
+        Visitor visitor9 = new Visitor("David", 28, "101 Road", "VIP", true);
+        Visitor visitor10 = new Visitor("Jack", 35, "202 Lane", "Regular", false);
+
+        // 添加Visitor到队列
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+        rollerCoaster.addVisitorToQueue(visitor3);
+        rollerCoaster.addVisitorToQueue(visitor4);
+        rollerCoaster.addVisitorToQueue(visitor5);
+        rollerCoaster.addVisitorToQueue(visitor6);
+        rollerCoaster.addVisitorToQueue(visitor7);
+        rollerCoaster.addVisitorToQueue(visitor8);
+        rollerCoaster.addVisitorToQueue(visitor9);
+        rollerCoaster.addVisitorToQueue(visitor10);
+
+        // 打印队列中的所有Visitor
+        rollerCoaster.printQueue();
+
+        // 运行一个周期
+        rollerCoaster.runOneCycle();
+
+        // 打印运行一个周期后的队列中的所有Visitor
+        rollerCoaster.printQueue();
+
+        // 打印集合中的所有Visitor
+        rollerCoaster.printRideHistory();
     }
 
     public void partSix(){
