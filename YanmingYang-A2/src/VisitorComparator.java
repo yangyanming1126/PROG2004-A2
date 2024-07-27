8895af415c3710e96bab1e8eb5abfd7dde3c4830
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-07-27 15:24:45 +1000

    updateVC

--- YanmingYang-A2/src/VisitorComparator.java
+++ YanmingYang-A2/src/VisitorComparator.java
@@ -1,2 +1,15 @@
-public class VisitorComparator {
+import java.util.Comparator;
+
+public class VisitorComparator implements Comparator<Visitor> {
+    @Override
+    public int compare(Visitor v1, Visitor v2) {
+        // 首先根据姓名进行比较
+        int nameComparison = v1.getName().compareTo(v2.getName());
+        if (nameComparison != 0) {
+            return nameComparison;
+        }
+
+        // 如果姓名相同，则根据年龄进行比较
+        return Integer.compare(v1.getAge(), v2.getAge());
+    }
 }
