4c0b5ec54a1a6fc6f035b9c05cb4cdd07d072693
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-08-01 18:42:01 +1000

    Update RideInterface.java

--- YanmingYang-A2/src/RideInterface.java
+++ YanmingYang-A2/src/RideInterface.java
@@ -1,7 +1,28 @@
 public interface RideInterface {
+    /**
+     * Used to add visitors to the queue. It has a parameter of type Visitor
+     * @param visitor
+     */
     void addVisitorToQueue(Visitor visitor);
+
+    /**
+     * Used to remove a guest from the queue. It has a parameter of type Visitor
+     * @param visitor
+     */
     void removeVisitorFromQueue(Visitor visitor);
+
+    /**
+     * Used to print a list of visitors waiting in the queue
+     */
     void printQueue();
+
+    /**
+     * A cycle used to run the ride
+     */
     void runOneCycle();
+
+    /**
+     * Used to print a list of visitors to the ride
+     */
     void printRideHistory();
 }
